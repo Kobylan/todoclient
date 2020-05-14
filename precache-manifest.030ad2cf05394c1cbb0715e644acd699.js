3c3bc59798a8a36ae3687d81b13cfeb8b131b946
kobylan <kobylan.kaz@gmail.com>
2020-05-14 20:23:51 +0600

    Updates

---
+++ precache-manifest.030ad2cf05394c1cbb0715e644acd699.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8241c3901d5fcb5f70a4a93465a4d113",
+    "url": "/todoclient/index.html"
+  },
+  {
+    "revision": "3a468155d61e51155bc0",
+    "url": "/todoclient/static/css/2.2bd37e2f.chunk.css"
+  },
+  {
+    "revision": "3a468155d61e51155bc0",
+    "url": "/todoclient/static/js/2.426641a9.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/todoclient/static/js/2.426641a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48fb920cb86eed525c42",
+    "url": "/todoclient/static/js/main.ee8c4529.chunk.js"
+  },
+  {
+    "revision": "0f57a75c7cccc805d738",
+    "url": "/todoclient/static/js/runtime-main.bb8458a7.js"
+  }
+]);
